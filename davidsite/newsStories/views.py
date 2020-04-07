7678b498293a56f1d067928b5f193db345530166
David McIvor <2510167M@student.gla.ac.uk>
2020-04-07 16:44:44 +0100

    3 articles

--- davidsite/newsStories/views.py
+++ davidsite/newsStories/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.template import loader
 from .models import NewsArticle
@@ -6,15 +7,14 @@ from .models import NewsArticle
 
 def index(request):
     latest_articles_list = NewsArticle.objects.order_by('-pub_date_time')[:5]
-    template = loader.get_template('newsStories/index.html')
-    context = {
-        'latest_article_list': latest_articles_list,
-    }
-    return HttpResponse(template.render(context, request))
+    #    template = loader.get_template('newsStories/index.html')
+    context = {'latest_article_list': latest_articles_list}
+    return render(request, 'newsStories/index.html', context)
 
 
-def detail(request, NewsArticle_id):
-    return HttpResponse("You're looking at question %s." % NewsArticle_id)
+def detail(request, NewsArtice_id):
+    article = get_object_or_404(NewsArticle, pk=NewsArtice_id)
+    return render(request, 'newsStories/index.html', {'NewsArticle': article})
 
 
 def results(request, NewsArticle_id):
