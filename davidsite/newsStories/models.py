e070302852325b0a718588945b88d2987b95039b
David McIvor <2510167M@student.gla.ac.uk>
2020-04-07 16:05:17 +0100

    Before adding templates

--- davidsite/newsStories/models.py
+++ davidsite/newsStories/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class NewsArticle(models.Model):
+    heading_text = models.CharField(max_length=50)
+    pub_date_time = models.DateTimeField('date published')
+    article_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.heading_text
+
+    def was_published_recently(self):
+        return self.pub_date_time >= timezone.now() - datetime.timedelta(days=1)
+
+#     def __init__(self):
+#        return self.article_text
--- davidsite/newsStories/views.py
+++ davidsite/newsStories/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import NewsArticle
 
-# Create your views here.
+
+def index(request):
+    latest_articles_list = NewsArticle.objects.order_by('-pub_date')[:5]
+    output = ', '.join([n.heading_text for n in latest_articles_list])
+    return HttpResponse(output)
+
+def detail(request, NewsArticle_id):
+    return HttpResponse("You're looking at question %s." % NewsArticle_id)
+
+def results(request, NewsArticle_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % NewsArticle_id)
+
+def vote(request, NewsArticle_id):
+    return HttpResponse("You're voting on question %s." % NewsArticle_id)
