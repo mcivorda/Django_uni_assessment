f4c14ff310ba619e206b6d1bb219b165a36f97ce
David McIvor <2510167M@student.gla.ac.uk>
2020-04-07 16:22:57 +0100

    Simple Template

--- davidsite/newsStories/views.py
+++ davidsite/newsStories/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import NewsArticle
 
 
 def index(request):
-    latest_articles_list = NewsArticle.objects.order_by('-pub_date')[:5]
-    output = ', '.join([n.heading_text for n in latest_articles_list])
-    return HttpResponse(output)
+    latest_articles_list = NewsArticle.objects.order_by('-pub_date_time')[:5]
+    template = loader.get_template('newsStories/index.html')
+    context = {
+        'latest_article_list': latest_articles_list,
+    }
+    return HttpResponse(template.render(context, request))
+
 
 def detail(request, NewsArticle_id):
     return HttpResponse("You're looking at question %s." % NewsArticle_id)
 
+
 def results(request, NewsArticle_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % NewsArticle_id)
+
 
 def vote(request, NewsArticle_id):
     return HttpResponse("You're voting on question %s." % NewsArticle_id)
